7c192aad067747785c424b4100cc3eb854436ccd
dendiflet <bretin.iannis@gmail.com>
2019-01-30 18:47:05 +0100

    ajout d'une entrée manuelle dans les tables

---
+++ db/migrate/20190130170140_create_pins.rb
@@ -0,0 +1,10 @@
+class CreatePins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pins do |t|
+      t.text :pins_name
+      t.text :url
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+  end
+end
